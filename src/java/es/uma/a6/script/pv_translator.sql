de5f13c5723054841076db6be1535078f4516f6e
Carlos <Carlos@192.168.8.116>
2018-11-02 13:11:13 +0100

    Operaciones de consulta añadidas

--- src/java/es/uma/a6/script/pv_translator.sql
+++ src/java/es/uma/a6/script/pv_translator.sql
@@ -19,20 +19,21 @@ CREATE TABLE campaña (
 );
 
 INSERT INTO modulo VALUES
-('moduo1', 0.5, 3, 5.3, 6),
-('moduo2', 0.7, 1, 4.3, 3),
-('moduo4', 0.9, 2, 5.9, 2);
+('modulo1', 0.5, 3, 5.3, 6),
+('modulo2', 0.7, 1, 4.3, 3),
+('modulo4', 0.9, 2, 5.9, 2);
 
 INSERT INTO campaña VALUES
-('campaña11', 'modulo1'),
-('campaña12', 'modulo1'),
-('campaña13', 'modulo1'),
-('campaña21', 'modulo2'),
-('campaña22', 'modulo2'),
-('campaña23', 'modulo2'),
-('campaña24', 'modulo2'),
-('campaña41', 'modulo4'),
-('campaña42', 'modulo4'),
-('campaña43', 'modulo4');
+('campaña11', '2018-02-02','modulo1'),
+('campaña12', '2017-03-04','modulo1'),
+('campaña13', '2018-04-06','modulo1'),
+('campaña21', '2018-04-12','modulo2'),
+('campaña22', '2018-06-14','modulo2'),
+('campaña23', '2018-07-22','modulo2'),
+('campaña24', '2016-06-29','modulo2'),
+('campaña41', '2018-07-28','modulo4'),
+('campaña42', '2014-12-14','modulo4'),
+('campaña43', '2018-11-03','modulo4');
+
 
 
